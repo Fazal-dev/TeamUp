505e75babdf6c3a3c94877d7e0bb8f1ab4458dca
Shain922 <fmhd48763@gmail.com>
2024-03-07 21:37:22 +0530

    add:connect to database

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,2 +1,10 @@
-export const PORT = 5555;
-export const mongoDBURL = `mongodb+srv://mhdm8675:HNDftNwbfwLQdsxc@books-store.y6y7iht.mongodb.net/books-collection?retryWrites=true&w=majority`;
+import mongoose from "mongoose";
+export const dbConnection = async () => {
+  const DB_URL = "mongodb://127.0.0.1:27017/TeamUp";
+  try {
+    await mongoose.connect(DB_URL, {});
+    console.log("CONNECTED TO DATABASE SUCCESSFULLY");
+  } catch (error) {
+    console.error("COULD NOT CONNECT TO DATABASE:", error.message);
+  }
+};
--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,23 @@
 import express from "express";
-const app = express();
 import cors from "cors";
+import { dbConnection } from "./db/index.js";
+const app = express();
+
 // middleware for parsing request body
 app.use(express.json());
 app.use(cors());
+
+try {
+  dbConnection();
+  app.listen(8000, (req, res) => {
+    console.log("server is running on 8000");
+  });
+} catch (error) {
+  console.error("COULD NOT CONNECT TO DATABASE:", error.message);
+}
 
 app.get("/", function (req, res) {
   res.send("hello world");
 });
 // routes
 // app.use("/books", booksRoute);
-
-app.listen(8000, (req, res) => {
-  console.log("server is running");
-});
