aa4848482c158b17e05c560420c9f1469889cad8
Shain922 <fmhd48763@gmail.com>
2024-04-21 18:26:58 +0530

    fix : improbe error handling

--- frontEnd/src/components/forms/LoginForm.jsx
+++ frontEnd/src/components/forms/LoginForm.jsx
@@ -1,27 +1,29 @@
 import React from "react";
-import {
-  Grid,
-  Typography,
-  TextField,
-  Button,
-  Link,
-  Alert,
-} from "@mui/material";
+import { Grid, Typography, TextField, Button, Link } from "@mui/material";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useSnackbar } from "notistack";
-
+import IconButton from "@mui/material/IconButton";
+import InputAdornment from "@mui/material/InputAdornment";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 const LoginForm = () => {
   const navigate = useNavigate("/");
-  const [error, setError] = useState(null);
+  const [errors, setErrors] = useState({ email: null, password: null });
   const { enqueueSnackbar } = useSnackbar();
   const [loading, setLoading] = useState();
   const [formData, setFormData] = useState({
     email: "",
     password: "",
   });
+  const [showPassword, setShowPassword] = useState(false);
 
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
   // login functoin
   const login = async () => {
     try {
@@ -29,6 +31,7 @@ const LoginForm = () => {
         "http://localhost:8000/api/user/login",
         formData
       );
+
       const token = user.data.token;
 
       // store token in local storage
@@ -50,27 +53,49 @@ const LoginForm = () => {
     } catch (error) {
       console.log(error);
       // error
-      setError(error.response.data.error);
+      setErrors({
+        ...errors,
+        email: error.response.data.error.email || null,
+        password: error.response.data.error.password || null,
+      });
     }
   };
 
   const handleChange = ({ currentTarget: input }) => {
     setFormData({ ...formData, [input.name]: input.value });
+    setErrors({ ...errors, [input.name]: null });
   };
-
+  // Function to validate email format
+  const validateEmail = (email) => {
+    // Regular expression for email validation
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
   const handleSubmit = async (e) => {
     e.preventDefault();
     setLoading(true);
+    // Validating email format
+    if (!validateEmail(formData.email)) {
+      setErrors({
+        ...errors,
+        email: "Please enter a valid email address.",
+      });
+      setLoading(false);
+      return;
+    }
     // Validate password length
     const { password } = formData;
+
     const isPasswordValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/.test(
       password
     );
 
     if (!isPasswordValid) {
-      setError(
-        "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, and one digit."
-      );
+      setErrors({
+        ...errors,
+        password:
+          "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, and one digit.",
+      });
       setLoading(false);
       return;
     }
@@ -80,7 +105,6 @@ const LoginForm = () => {
       await login();
       setLoading(false);
     } catch (error) {
-      setError("An unexpected error occurred. Please try again later.");
       setLoading(false);
       console.error("Login failed:", error);
     }
@@ -93,11 +117,6 @@ const LoginForm = () => {
           <Grid item xs={12} textAlign={"center"}>
             <Typography variant="h5">Sign In</Typography>
           </Grid>
-          {error && (
-            <Grid xs={12} item sx={{ textAlign: "center" }}>
-              <Alert severity="error">{error}</Alert>
-            </Grid>
-          )}
           <Grid item xs={12}>
             <TextField
               label="Email"
@@ -107,18 +126,34 @@ const LoginForm = () => {
               required
               onChange={handleChange}
               value={formData.email}
+              error={errors.email !== null}
+              helperText={errors.email}
             />
           </Grid>
           <Grid item xs={12}>
             <TextField
+              type={showPassword ? "text" : "password"}
+              endadornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
+                  </IconButton>
+                </InputAdornment>
+              }
               label="Password"
               variant="outlined"
               name="password"
               required
               onChange={handleChange}
               value={formData.password}
-              type="password"
               fullWidth
+              error={errors.password !== null}
+              helperText={errors.password}
             />
           </Grid>
           <Grid item xs={12}>
