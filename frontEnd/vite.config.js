1060769fade6c76d5ded69f073370c8ff895eed5
Shain922 <fmhd48763@gmail.com>
2024-07-20 19:12:43 +0530

    vercel json to front end

--- frontEnd/vite.config.js
+++ frontEnd/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/",
   optimizeDeps: {
     include: ["@emotion/react", "@emotion/styled", "@mui/material/Tooltip"],
   },
